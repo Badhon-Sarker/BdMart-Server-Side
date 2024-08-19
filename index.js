5e3481cee6d809d619a82bb0f42df71c11fb013f
Badhon-Sarker <badhonsarker38@gmail.com>
2024-08-19 17:00:46 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,29 @@
+const express = require('express')
+const cors = require('cors')
+require('dotenv').config()
+const app = express()
+const port = 5000
+
+
+// middleware
+
+app.use(cors({
+    origin: [
+        'http://localhost:5173',
+        'http://localhost:5174',
+        'https://assignment-12-93c77.web.app'
+        
+      ],
+      credentials: true,
+      optionSuccessStatus: 200,
+}))
+app.use(express.json())
+
+
+app.get('/', (req, res) => {
+  res.send('Hello bro!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening on port ${port}`)
+})
